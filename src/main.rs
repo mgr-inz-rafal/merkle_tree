187bb7f8b16eace8765e36acc769c75cbc682ad9
mgr_inz_rafal <rchabowski@gmail.com>
2024-02-09 22:08:49 +0100

    Sparse implementation

--- src/main.rs
+++ src/main.rs
@@ -1,144 +1,89 @@
-use core::fmt;
-use std::fmt::Display;
+use std::{
+    collections::hash_map::DefaultHasher,
+    fmt::Debug,
+    hash::{Hash, Hasher},
+};
 
-struct Node<T> {
-    is_fake: bool,
-    data: T,
+#[derive(Debug)]
+struct MerkleTree {
+    hashes: Vec<String>,
 }
 
-impl<T> Node<T> {
-    fn new(data: T) -> Self {
+impl MerkleTree {
+    fn new(leaf_count: usize) -> Self {
+        assert!(
+            is_power_of_two(leaf_count),
+            "leaf count should be a power of 2"
+        );
+
         Self {
-            is_fake: false,
-            data,
+            hashes: vec!["-".to_string(); leaf_count * 2],
         }
     }
 
-    fn new_fake(data: T) -> Self {
-        Self {
-            is_fake: true,
-            data,
-        }
+    fn len(&self) -> usize {
+        self.hashes.len() / 2
+    }
+
+    fn set_at<T>(&mut self, index: usize, item: T)
+    where
+        T: Hash,
+    {
+        let mut hasher = DefaultHasher::new();
+        item.hash(&mut hasher);
+        let my_hash = hasher.finish().to_string();
+
+        let i = index + self.len();
+        self.hashes[i] = my_hash.clone();
+
+        let sibling = sibling_index(i);
+        let sibling_hash = &self.hashes[sibling];
+
+        let concat_hash = format!("{}{}", my_hash, sibling_hash);
+        let mut hasher = DefaultHasher::new();
+        concat_hash.hash(&mut hasher);
+        let parent_hash = hasher.finish().to_string();
+
+        let parent = parent_index(i);
+        self.hashes[parent] = parent_hash;
     }
 }
 
-impl<T> From<T> for Node<T> {
-    fn from(value: T) -> Self {
-        Node::new(value)
+fn is_power_of_two(n: usize) -> bool {
+    if n == 0 {
+        false
+    } else {
+        n & (n - 1) == 0
     }
 }
 
-impl<T: Display> fmt::Display for Node<T> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}{}", self.data, if self.is_fake { '-' } else { ' ' })?;
-        Ok(())
+fn parent_index(index: usize) -> usize {
+    if index % 2 == 0 {
+        index / 2
+    } else {
+        (index - 1) / 2
     }
 }
 
-struct MerkleTree<T: Default> {
-    nodes: Vec<Node<T>>,
-}
-
-impl<T: Default> MerkleTree<T> {
-    pub fn _new(data: T) -> Self {
-        Self {
-            nodes: vec![Node::new_fake(T::default()), Node::new(data)],
-        }
-    }
-
-    pub fn from_iter(i: impl IntoIterator<Item = T>) -> Self {
-        Self {
-            nodes: std::iter::once(Node::new_fake(T::default()))
-                .chain(i.into_iter().map(|v| Node::new(v)))
-                .collect(),
-        }
-    }
-
-    pub fn _add(&mut self, node: Node<T>) {
-        self.nodes.push(node)
-    }
-
-    pub fn height(&self) -> u32 {
-        self.nodes.len().ilog2()
-    }
-
-    pub fn level_of(&self, i: usize) -> u32 {
-        self.height() - if i == 0 { 0 } else { i.ilog2() }
-    }
-
-    pub fn len(&mut self) -> usize {
-        self.nodes.len()
-    }
-
-    pub fn node(&self, i: usize) -> Option<&Node<T>> {
-        self.nodes.get(i)
-    }
-}
-
-impl<T> fmt::Display for MerkleTree<T>
-where
-    T: Default + Display,
-{
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        writeln!(
-            f,
-            "total nodes = {}, height = {}",
-            self.nodes.len(),
-            self.height()
-        )?;
-        let mut current_level: u32 = u32::MAX;
-        for (i, n) in self.nodes.iter().enumerate().skip(1) {
-            let level_of = self.level_of(i);
-            if current_level != level_of {
-                current_level = level_of;
-                writeln!(f)?;
-            }
-            write!(f, "{}", n)?;
-        }
-        Ok(())
+fn sibling_index(index: usize) -> usize {
+    if index % 2 == 0 {
+        index + 1
+    } else {
+        index - 1
     }
 }
 
 fn main() {
-    //                 A                  level 3
-    //          B             C           level 2
-    //      D      E      F      G        level 1
-    //    H   I  J  .   .   .  .   .      level 0
+    let chars = "ABCD";
+    chars.chars().for_each(|c| {
+        let mut hasher = DefaultHasher::new();
+        c.hash(&mut hasher);
+        println!("{} - {}", c, hasher.finish());
+    });
 
-    let mut mt = MerkleTree::from_iter("ABCDEFGHIJ".chars());
-    println!("{}", mt);
+    let mut mt = MerkleTree::new(4);
 
-    for i in 1..mt.len() {
-        let node = mt.node(i).unwrap();
-        let parent = mt.node(i / 2).unwrap();
-        println!("{} (level:{}) -> {}", node, mt.level_of(i), parent);
-    }
-}
+    mt.set_at(1, 'A');
 
-#[cfg(test)]
-mod tests {
-    use crate::MerkleTree;
-
-    #[test]
-    fn should_calculate_height() {
-        let mt = MerkleTree::from_iter("ABCDEFGHIJ".chars());
-
-        assert_eq!(mt.height(), 3)
-    }
-
-    #[test]
-    fn should_calculate_level_of_item() {
-        let mt = MerkleTree::from_iter("ABCDEFGHIJ".chars());
-
-        assert_eq!(mt.level_of(1), 3);
-        assert_eq!(mt.level_of(2), 2);
-        assert_eq!(mt.level_of(3), 2);
-        assert_eq!(mt.level_of(4), 1);
-        assert_eq!(mt.level_of(5), 1);
-        assert_eq!(mt.level_of(6), 1);
-        assert_eq!(mt.level_of(7), 1);
-        assert_eq!(mt.level_of(8), 0);
-        assert_eq!(mt.level_of(9), 0);
-        assert_eq!(mt.level_of(10), 0);
-    }
+    dbg!(&mt);
 }
